1efcd4126f5090fb8059955a3abad9f55bebbb72
Subrata Guchhait <116452735+Subrata-Hub@users.noreply.github.com>
2024-05-29 13:49:49 +0530

    Update openAI.js

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: OPENAI_KEY,
+  apiKey: "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",
 
   dangerouslyAllowBrowser: true,
 });
