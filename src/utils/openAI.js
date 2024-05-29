c92958cf68cfb88021da81b272c399c1804f4d9c
Subrata Guchhait <sguchhaitdev@gmail.com>
2024-05-29 13:15:13 +0530

    readme

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: OPENAI_KEY,
 
   dangerouslyAllowBrowser: true,
 });
