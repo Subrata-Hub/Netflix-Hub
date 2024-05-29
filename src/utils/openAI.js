ae1c68649c1e9de029e776f5676b2ab7ff954433
Subrata Guchhait <sguchhaitdev@gmail.com>
2024-05-29 14:14:21 +0530

    kkk

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,10 +1,10 @@
 import OpenAI from "openai";
 // import { OPENAI_KEY } from "./constants";
 
-const openAIKey = process.env.OPENAI_API_KEY;
+// const openAIKey = process.env.REACT_APP_OPENAI_KEY;
 
 const openai = new OpenAI({
-  apiKey: openAIKey,
+  apiKey: "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",
 
   dangerouslyAllowBrowser: true,
 });
